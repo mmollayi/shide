f89b62743e0a175b213753f82a1d6e49e644cf3b
Mohsen <mmollayi@gmail.com>
2024-02-05 15:07:48 +0330

    refactor and update getters.R
    names are  now retained when using getter functions for jdate and jdatetime

--- R/getters.R
+++ R/getters.R
@@ -2,21 +2,23 @@ tzone <- function(x) {
     attr(x, "tzone")[[1]] %||% ""
 }
 
+jdate_get_field <- function(x, field) {
+    fields <- jdate_get_fields_cpp(x)
+    out <- fields[[field]]
+    names(out) <- names(x)
+    out
+}
+
+jdatetime_get_field <- function(x, field) {
+    fields <- jdatetime_get_fields_cpp(x)
+    out <- fields[[field]]
+    names(out) <- names(x)
+    out
+}
+
 #' @export
 sh_year <- function(x) {
     UseMethod("sh_year")
-}
-
-#' @export
-sh_year.jdate <- function(x) {
-    fields <- jdate_get_fields_cpp(x)
-    fields$year
-}
-
-#' @export
-sh_year.jdatetime <- function(x) {
-    fields <- jdatetime_get_fields_cpp(x)
-    fields$year
 }
 
 #' @export
@@ -25,32 +27,8 @@ sh_month <- function(x) {
 }
 
 #' @export
-sh_month.jdate <- function(x) {
-    fields <- jdate_get_fields_cpp(x)
-    fields$month
-}
-
-#' @export
-sh_month.jdatetime <- function(x) {
-    fields <- jdatetime_get_fields_cpp(x)
-    fields$month
-}
-
-#' @export
 sh_day <- function(x) {
     UseMethod("sh_day")
-}
-
-#' @export
-sh_day.jdate <- function(x) {
-    fields <- jdate_get_fields_cpp(x)
-    fields$day
-}
-
-#' @export
-sh_day.jdatetime <- function(x) {
-    fields <- jdatetime_get_fields_cpp(x)
-    fields$day
 }
 
 #' @export
@@ -59,24 +37,8 @@ sh_doy <- function(x) {
 }
 
 #' @export
-sh_doy.jdate <- function(x) {
-    jdate_get_doy_cpp(x)
-}
-
-#' @export
-sh_doy.jdatetime <- function(x) {
-    jdate_get_doy_cpp(as_jdate(x))
-}
-
-#' @export
 sh_hour <- function(x) {
     UseMethod("sh_hour")
-}
-
-#' @export
-sh_hour.jdatetime <- function(x) {
-    fields <- jdatetime_get_fields_cpp(x)
-    fields$hour
 }
 
 #' @export
@@ -85,18 +47,65 @@ sh_minute <- function(x) {
 }
 
 #' @export
-sh_minute.jdatetime <- function(x) {
-    fields <- jdatetime_get_fields_cpp(x)
-    fields$minute
-}
-
-#' @export
 sh_second <- function(x) {
     UseMethod("sh_second")
 }
 
 #' @export
+sh_year.jdate <- function(x) {
+    jdate_get_field(x, "year")
+}
+
+#' @export
+sh_year.jdatetime <- function(x) {
+    jdatetime_get_field(x, "year")
+}
+
+#' @export
+sh_month.jdate <- function(x) {
+    jdate_get_field(x, "month")
+}
+
+#' @export
+sh_month.jdatetime <- function(x) {
+    jdatetime_get_field(x, "month")
+}
+
+#' @export
+sh_day.jdate <- function(x) {
+    jdate_get_field(x, "day")
+}
+
+#' @export
+sh_day.jdatetime <- function(x) {
+    jdatetime_get_field(x, "day")
+}
+
+#' @export
+sh_doy.jdate <- function(x) {
+    out <- jdate_get_doy_cpp(x)
+    names(out) <- names(x)
+    out
+}
+
+#' @export
+sh_doy.jdatetime <- function(x) {
+    out <- jdate_get_doy_cpp(as_jdate(x))
+    names(out) <- names(x)
+    out
+}
+
+#' @export
+sh_hour.jdatetime <- function(x) {
+    jdatetime_get_field(x, "hour")
+}
+
+#' @export
+sh_minute.jdatetime <- function(x) {
+    jdatetime_get_field(x, "minute")
+}
+
+#' @export
 sh_second.jdatetime <- function(x) {
-    fields <- jdatetime_get_fields_cpp(x)
-    fields$second
+    jdatetime_get_field(x, "second")
 }
