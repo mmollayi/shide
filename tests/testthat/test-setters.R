ae8e0b39b12de1e7e469964f658e5584b56f98b5
Mohsen <mmollayi@gmail.com>
2024-02-26 15:53:14 +0330

    add tests for setter functions

---
+++ tests/testthat/test-setters.R
@@ -0,0 +1,36 @@
+test_that("setter functions work as expected", {
+    dt <- jdatetime("1402-12-07 15:03:15", tz = "Asia/Tehran")
+    d <- as_jdate(dt)
+
+    sh_second(dt) <- 20:21
+    expect_equal(sh_second(dt), 20:21)
+    expect_error(sh_second(d) <- 20)
+
+    sh_minute(dt) <- c(1, 10)
+    expect_equal(sh_minute(dt), c(1, 10))
+    expect_error(sh_minute(d) <- 1)
+
+    sh_hour(dt) <- 12
+    expect_equal(sh_hour(dt), c(12, 12))
+    expect_error(sh_hour(d) <- 12)
+
+    sh_day(d) <- c(17, 27)
+    sh_day(dt) <- c(17, 27)
+    expect_equal(sh_day(d), c(17, 27))
+    expect_equal(sh_day(dt), c(17, 27))
+
+    sh_mday(d) <- c(15, 25)
+    sh_mday(dt) <- c(15, 25)
+    expect_equal(sh_mday(d), c(15, 25))
+    expect_equal(sh_mday(dt), c(15, 25))
+
+    sh_month(d) <- c(2, 11)
+    sh_month(dt) <- c(2, 11)
+    expect_equal(sh_month(d), c(2, 11))
+    expect_equal(sh_month(dt), c(2, 11))
+
+    sh_year(d) <- c(1367, 1403)
+    sh_year(dt) <- c(1367, 1403)
+    expect_equal(sh_year(d), c(1367, 1403))
+    expect_equal(sh_year(dt), c(1367, 1403))
+})
