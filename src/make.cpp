8a7ca4e5f4cc1ed6a3826ad0e75e92626d842089
Mohsen <mmollayi@gmail.com>
2024-12-28 23:11:04 +0330

    add make.h

--- src/make.cpp
+++ src/make.cpp
@@ -1,102 +1,9 @@
 #include "shide.h"
-#include <map>
-#include <math.h>
-#include <optional>
-#include "hour_minute_second.h"
+#include "make.h"
 
 using cpp11::integers;
 using cpp11::doubles;
 using std::chrono::seconds;
-
-enum class choose { earliest, latest, NA };
-
-std::optional<choose> string_to_choose(const std::string& choose_str) {
-    static const std::map<std::string, choose> choose_map{
-        {"earliest", choose::earliest},
-        {"latest", choose::latest},
-        {"NA", choose::NA},
-    };
-
-    auto it = choose_map.find(choose_str);
-    if (it != choose_map.end()) {
-        return it->second;
-    }
-    else {
-        return {};
-    }
-}
-
-choose detect_ambiguous_resolution_from_reference(const date::time_zone* tz,
-                                                  const date::sys_seconds& ss_ref)
-{
-    date::sys_info info;
-    tzdb::get_sys_info(ss_ref, tz, info);
-    const date::local_seconds ls_ref{ (ss_ref + info.offset).time_since_epoch() };
-    date::local_info info2;
-    tzdb::get_local_info(ls_ref, tz, info2);
-
-    if (info2.first.begin == info.begin)
-    {
-        return choose::earliest;
-    }
-    else if (info2.second.begin == info.begin)
-    {
-        return choose::latest;
-    }
-    else
-    {
-        return choose::NA;
-    }
-}
-
-double jdatetime_from_local_seconds(const date::local_seconds& ls, const date::time_zone* tz,
-                                    date::local_info& info, choose c,
-                                    const date::sys_seconds* ss_ref=nullptr)
-{
-    tzdb::get_local_info(ls, tz, info);
-    seconds s{};
-    if (info.result == date::local_info::unique)
-    {
-        s = ls.time_since_epoch() - info.first.offset;
-    }
-    else if (info.result == date::local_info::ambiguous)
-    {
-        if (ss_ref)
-            c = detect_ambiguous_resolution_from_reference(tz, *ss_ref);
-        switch (c)
-        {
-        case choose::earliest:
-            s = ls.time_since_epoch() - info.first.offset;
-            break;
-        case choose::latest:
-            s = ls.time_since_epoch() - info.second.offset;
-            break;
-        case choose::NA:
-            return NA_REAL;
-        }
-    }
-    else
-    {
-        return NA_REAL;
-    }
-
-    return static_cast<double>(s.count());
-}
-
-bool make_local_seconds(const sh_fields& fds, date::local_seconds& ls)
-{
-    if (!fds.tod.in_conventional_range()) {
-        return false;
-    }
-
-    if (!fds.ymd.ok())
-    {
-        return false;
-    }
-
-    ls = local_days(fds.ymd) + fds.tod.to_duration();
-    return true;
-}
 
 [[cpp11::register]]
 doubles jdate_make_cpp(cpp11::list_of<cpp11::integers> fields) {
@@ -153,13 +60,7 @@ jdatetime_make_impl(const integers& year, const integers& month, const integers&
 
         fds.ymd = {date::year(year[i]), date::month(month[i]), date::day(day[i])};
         fds.tod = {hours(hour[i]), minutes(minute[i]), seconds(second[i])};
-        if (!make_local_seconds(fds, ls))
-        {
-            out[i] = NA_REAL;
-            continue;
-        }
-
-        out[i] = jdatetime_from_local_seconds(ls, tz, info, c);
+        out[i] = make_jdatetime(fds, tz, info, c);
     }
 
     return out;
@@ -209,14 +110,8 @@ doubles jdatetime_make_with_reference_impl(const integers& year, const integers&
 
         fds.ymd = {date::year(year[i]), date::month(month[i]), date::day(day[i])};
         fds.tod = {hours(hour[i]), minutes(minute[i]), seconds(second[i])};
-        if (!make_local_seconds(fds, ls))
-        {
-            out[i] = NA_REAL;
-            continue;
-        }
-
         ss_ref = sys_seconds_from_double(ref[i]);
-        out[i] = jdatetime_from_local_seconds(ls, tz, info, choose{}, &ss_ref);
+        out[i] = make_jdatetime(fds, tz, info, ss_ref);
     }
 
     return out;
---
+++ src/make.h
@@ -0,0 +1,141 @@
+#ifndef MAKE_H
+#define MAKE_H
+#include <map>
+#include <optional>
+#include "sh_year_month_day.h"
+#include <tzdb/tzdb.h>
+
+using std::chrono::hours;
+using std::chrono::seconds;
+using std::chrono::minutes;
+using date::sys_seconds;
+
+enum class choose { earliest, latest, NA };
+
+std::optional<choose> string_to_choose(const std::string& choose_str)
+{
+    static const std::map<std::string, choose> choose_map{
+        {"earliest", choose::earliest},
+        {"latest", choose::latest},
+        {"NA", choose::NA},
+    };
+
+    auto it = choose_map.find(choose_str);
+    if (it != choose_map.end()) {
+        return it->second;
+    }
+    else {
+        return {};
+    }
+}
+
+choose sys_seconds_to_choose(const sys_seconds& tp, const date::time_zone* tz)
+{
+    date::sys_info info;
+    tzdb::get_sys_info(tp, tz, info);
+    const date::local_seconds ls_ref{ (tp + info.offset).time_since_epoch() };
+    date::local_info info2;
+    tzdb::get_local_info(ls_ref, tz, info2);
+
+    if (info2.first.begin == info.begin)
+    {
+        return choose::earliest;
+    }
+    else if (info2.second.begin == info.begin)
+    {
+        return choose::latest;
+    }
+    else
+    {
+        return choose::NA;
+    }
+}
+
+double jdatetime_from_local_seconds(const date::local_seconds& ls, const date::time_zone* tz,
+    date::local_info& info, choose c, const sys_seconds* ss_ref = nullptr)
+{
+    tzdb::get_local_info(ls, tz, info);
+    seconds s{};
+    if (info.result == date::local_info::unique)
+    {
+        s = ls.time_since_epoch() - info.first.offset;
+    }
+    else if (info.result == date::local_info::ambiguous)
+    {
+        if (ss_ref)
+            c = sys_seconds_to_choose(*ss_ref, tz);
+        switch (c)
+        {
+        case choose::earliest:
+            s = ls.time_since_epoch() - info.first.offset;
+            break;
+        case choose::latest:
+            s = ls.time_since_epoch() - info.second.offset;
+            break;
+        case choose::NA:
+            return NA_REAL;
+        }
+    }
+    else
+    {
+        return NA_REAL;
+    }
+
+    return static_cast<double>(s.count());
+}
+
+constexpr
+std::optional<date::local_seconds> make_local_seconds(const sh_fields& fds)
+{
+    if (!fds.tod.in_conventional_range())
+        return {};
+
+    if (!fds.ymd.ok())
+        return {};
+
+    return local_days(fds.ymd) + fds.tod.to_duration();
+}
+
+double make_jdatetime(const sh_fields& fds, const date::time_zone* tz,
+    date::local_info& info, choose c = choose::earliest)
+{
+    auto ls = make_local_seconds(fds);
+    if (!ls.has_value())
+        return NA_REAL;
+
+    return jdatetime_from_local_seconds(*ls, tz, info, c);
+}
+
+double make_jdatetime(const sh_fields& fds, const date::time_zone* tz,
+    date::local_info& info, const sys_seconds& ss_ref)
+{
+    auto ls = make_local_seconds(fds);
+    if (!ls.has_value())
+        return NA_REAL;
+
+    return jdatetime_from_local_seconds(*ls, tz, info, choose{}, &ss_ref);
+}
+
+double make_jdatetime(const sh_fields& fds, const std::string& tz_name,
+    choose c=choose::earliest)
+{
+    const date::time_zone* tz{};
+    if (!tzdb::locate_zone(tz_name, tz))
+        return NA_REAL;
+
+    date::local_info info;
+    return make_jdatetime(fds, tz, info, c);
+}
+
+double make_jdatetime(const sh_fields& fds, const std::string& tz_name,
+    const sys_seconds& ss_ref)
+{
+    const date::time_zone* tz{};
+    if (!tzdb::locate_zone(tz_name, tz))
+        return NA_REAL;
+
+    date::local_info info;
+    return make_jdatetime(fds, tz, info, ss_ref);
+}
+
+#endif
