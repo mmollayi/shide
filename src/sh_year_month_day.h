eb79e203f0f4c60ebf2e3b5dac190c5b5aee7e0f
Mohsen <mmollayi@gmail.com>
2024-12-31 13:27:44 +0330

    merged hour_minute_second.h into sh_year_month_day.h

--- src/sh_year_month_day.h
+++ src/sh_year_month_day.h
@@ -1,7 +1,6 @@
 #ifndef SH_YEAR_MONTH_DAY_H
 #define SH_YEAR_MONTH_DAY_H
 #include <tzdb/date.h>
-#include "hour_minute_second.h"
 
 namespace internal
 {
@@ -490,6 +489,50 @@ sh_year_month_day::sh_year_month_day(const sh_year_month_day_last& ymdl) NOEXCEP
 : y_(ymdl.year())
     , m_(ymdl.month())
     , d_(ymdl.day())
+{}
+
+class hour_minute_second
+{
+    std::chrono::hours h_;
+    std::chrono::minutes m_;
+    std::chrono::seconds s_;
+
+public:
+    constexpr hour_minute_second() noexcept
+        : hour_minute_second(std::chrono::seconds::zero())
+    {}
+
+    constexpr explicit hour_minute_second(std::chrono::seconds d) noexcept
+        : h_(std::chrono::duration_cast<std::chrono::hours>(std::chrono::abs(d)))
+    , m_(std::chrono::duration_cast<std::chrono::minutes>(std::chrono::abs(d)) - h_)
+    , s_(std::chrono::abs(d) - h_ - m_)
+    {}
+
+    constexpr hour_minute_second(const std::chrono::hours& h, const std::chrono::minutes& m,
+                                 const std::chrono::seconds& s) noexcept;
+
+    constexpr std::chrono::hours hours() const noexcept { return h_; }
+    constexpr std::chrono::minutes minutes() const noexcept { return m_; }
+    constexpr std::chrono::seconds seconds() const noexcept { return s_; }
+    constexpr std::chrono::seconds to_duration() const noexcept
+    {
+        return s_ + m_ + h_;
+    }
+
+    constexpr bool in_conventional_range() const noexcept
+    {
+        return h_ < date::days{ 1 } && m_ < std::chrono::hours{ 1 } &&
+            s_ < std::chrono::minutes{ 1 };
+    }
+};
+
+constexpr
+inline
+hour_minute_second::hour_minute_second(const std::chrono::hours& h, const std::chrono::minutes& m,
+                                       const std::chrono::seconds& s) noexcept
+    : h_(h)
+    , m_(m)
+    , s_(s)
 {}
 
 struct sh_fields
