4327ac4b1745ce65d1a3a7d48679f4ec45d8ee55
mmollayi <mmollayi@gmail.com>
2025-12-31 11:26:01 +0330

    add sh_format()

--- R/sh_format.R
+++ R/sh_format.R
@@ -6,8 +6,13 @@ sh_format <- function(x, format = NULL, labels = NULL, ...) {
 #' @export
 sh_format.jdate <- function(x, format = NULL, labels = NULL, ...) {
     format <- format %||% "%Y-%m-%d"
-    if (is.null(labels))
+    if (is.null(labels)) {
         labels <- shide_labels_default
+    } else {
+        if (!inherits(labels, "shide_labels")) {
+            cli::cli_abort("{.var labels} must be a {.cls shide_lables} object.")
+        }
+    }
 
     out <- format_jdate_cpp(x, format, labels$month,
                             labels$month_abbr, labels$weekday, labels$weekday_abbr)
@@ -18,8 +23,13 @@ sh_format.jdate <- function(x, format = NULL, labels = NULL, ...) {
 #' @export
 sh_format.jdatetime <- function(x, format = NULL, labels = NULL, ...) {
     format <- format %||% "%Y-%m-%d %T %z"
-    if (is.null(labels))
+    if (is.null(labels)) {
         labels <- shide_labels_default
+    } else {
+        if (!inherits(labels, "shide_labels")) {
+            cli::cli_abort("{.var labels} must be a {.cls shide_lables} object.")
+        }
+    }
 
     out <- format_jdatetime_cpp(x, format, labels$month, labels$month_abbr,
                                 labels$weekday, labels$weekday_abbr, labels$am_pm)
